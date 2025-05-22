1aff396f419f451d8cef257ba9e21332561bb91d
cappyhackky <saifizeeshan895@gmail.com>
2025-05-22 15:51:36 +0530

    Commit from server adding server/uploads directory in .gitignore

--- server/routes/UserProfile.js
+++ server/routes/UserProfile.js
@@ -1,23 +1,13 @@
 import express from "express";
 // import { fetchUser } from '../middleware/fetchUser.js';
 import { fetchUser } from '../middleware/fetchUser.js';
-<<<<<<< Updated upstream
 import { profileDetail, getUserDiscussion, deleteUserDiscussion, uploadUserAvatar } from "../controllers/userProfile.js";
-=======
-import { profileDetail, getUserDiscussion, deleteUserDiscussion, updateProfilePicture } from "../controllers/userProfile.js";
->>>>>>> Stashed changes
 
 const router = express.Router();
 
 router.post('/profileDetail', fetchUser, profileDetail)
 router.post('/getUserDiscussion', fetchUser, getUserDiscussion)
 router.post('/deleteUserDiscussion', fetchUser, deleteUserDiscussion)
-<<<<<<< Updated upstream
-router.post('/uploadUserAvatar', fetchUser, uploadUserAvatar)
-
-
-=======
-router.post('/updateProfilePicture', fetchUser, updateProfilePicture)
->>>>>>> Stashed changes
+router.post('/updateProfilePicture', fetchUser, uploadUserAvatar)
 
 export default router;
