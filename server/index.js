70e6b0b9820cc421490455566bd2b10af7bbf9ca
NILESH0005 <thakurnilesh2000@gmail.com>
2025-06-16 22:58:32 -0700

    merging submodule reloading take 2

--- server/index.js
+++ server/index.js
@@ -57,9 +57,9 @@ app.use('/quiz', quizRoutes);
 app.use('/lmsEdit', lmsEdit);
 app.use('/progressTrack', progressRoute )
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/dist')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
 });
 
 
